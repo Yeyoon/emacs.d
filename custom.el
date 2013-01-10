bd10cae911d8f613ee97ddbcfb71dfe4768c7f81
pluto <blueabysm@gmail.com>
2013-01-10 21:32:02 +0800

    remove looks

--- custom.el
+++ custom.el
@@ -40,13 +40,6 @@
 ;; truncate line
 (toggle-truncate-lines)
 
-;; custom mode line
-;; mode line
-(set-face-background 'mode-line-inactive "#222222")
-(set-face-foreground 'mode-line-inactive "#CCCCCC")
-(set-face-background 'mode-line "#FFFFFF")
-(set-face-foreground 'mode-line "#003300")
-
 ;; region background
 (set-face-background 'region "#444444")
 
