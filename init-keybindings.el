6fc3e24a1bd38cd60b154ec2ad44815fb83b41cc
pluto <blueabysm@gmail.com>
2013-01-28 11:33:42 +0800

    remove unnucessary key-bindings; add rsense

--- init-keybindings.el
+++ init-keybindings.el
@@ -1,7 +1,6 @@
 (global-set-key (kbd "C-c g") 'goto-line)
 (global-set-key (kbd "M-n") 'scroll-other-window)
 (global-set-key (kbd "M-p") 'scroll-other-window-down)
-(global-set-key (kbd "C-x c") 'comment-or-uncomment-region)
 (global-set-key (kbd "M-`") 'other-window)
 
 (global-set-key (kbd "C-/") 'undo)
--- init-ruby-mode.el
+++ init-ruby-mode.el
@@ -92,4 +92,16 @@
 ;; ruby arguments indentation
 (setq ruby-deep-indent-paren nil)
 
+;;----------------------------------------------------------------------------
+;; Rsense Configurations
+;;----------------------------------------------------------------------------
+(setq rsense-home (expand-file-name "~/.emacs.d/site-lisp/rsense"))
+(add-to-list 'load-path (concat rsense-home "/etc"))
+(require 'rsense)
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (add-to-list 'ac-sources 'ac-source-rsense-method)
+            (add-to-list 'ac-sources 'ac-source-rsense-constant)))
+
 (provide 'init-ruby-mode)
