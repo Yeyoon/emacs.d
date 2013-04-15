aa5d42e4b07ec02ad835b736e4ab813a758498fc
wanglei <wanglei102891@pwrd.com>
2013-04-15 16:46:02 +0800

    adjustment

--- init-php.el
+++ init-php.el
@@ -11,15 +11,4 @@
          (regexp-opt
           '("php" "php.example" "inc"))
          "\\'"))
-
-(defun browse-url-in-other-window (url &optional newwin)
-  "Open url in other window"
-  (message "Opening PHP online manual...")
-  (let ((pop-up-windows t))
-    (if (one-window-p) (split-window-sensibly (frame-selected-window)))
-    (other-window 1)
-    (w3m-browse-url url newwin)))
-
-(setq browse-url-browser-function 'browse-url-in-other-window)
-
 (provide 'init-php)
--- init-w3m.el
+++ init-w3m.el
@@ -1,5 +1,23 @@
 (require-package 'w3m)
 
-(setq browse-url-browser-function 'w3m-browse-url)
-(global-set-key (kbd "C-c C-l") browse-url-browser-function)
+(setq browse-url-browser-function 'browse-url-in-other-window-without-focus-on)
+
+(defun browse-url-in-other-window (url &optional newwin)
+  "Open url in other window"
+  (message "Loading...")
+  (let ((w3m-pop-up-windows t))
+    (if (one-window-p) (split-window-sensibly (frame-selected-window)))
+    (other-window 1)
+    (w3m-browse-url url newwin)))
+
+(defun browse-url-in-other-window-without-focus-on (url &optional newwin)
+  "Open url in other window without move focus on it"
+  (message "Loading...")
+  (let ((w3m-pop-up-windows t))
+    (if (one-window-p) (split-window-sensibly (frame-selected-window)))
+    (other-window 1)
+    (w3m-browse-url url newwin)
+    (other-window -1)))
+
+(global-set-key (kbd "C-c C-l") 'browse-url)
 (provide 'init-w3m)
